52acd0007161b697c42240b9e65f82b95eca5f6f
vasilyevr <romanvasilyev@gmail.com>
2021-01-28 01:54:27 +0300

    Fix some issues

--- src/main.php
+++ src/main.php
@@ -16,12 +16,12 @@ use TgBotApi\BotApiBase\BotApi;
 use TgBotApi\BotApiBase\BotApiNormalizer;
 
 if (empty($BOT_API_KEY)) {
-    print_r('Please create BOT_API_KEY constant in config/parameters.php');
+    echo 'Please create BOT_API_KEY constant in config/parameters.php';
     return;
 }
 
 if (empty($TIME_OUT_PUZZLE_REPLY)) {
-    print_r('Please create TIME_OUT_PUZZLE_REPLY constant in config/parameters.php');
+    echo 'Please create TIME_OUT_PUZZLE_REPLY constant in config/parameters.php';
     return;
 }
 
