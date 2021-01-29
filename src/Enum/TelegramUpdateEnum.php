10d10513ecff064e2ccd60bb28aa880ab218c099
VasilyevR <romanvasilyev@gmail.com>
2021-01-29 20:17:26 +0300

    Add TelegramUpdateEnum

---
+++ src/Enum/TelegramUpdateEnum.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Enum;
+
+class TelegramUpdateEnum
+{
+    public const NEW_MEMBER = 'new_member';
+    public const PUZZLE_ANSWER = 'puzzle_answer';
+}
